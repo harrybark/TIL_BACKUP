4f78d55e2b72a5e0b2b66779d3c5c8340c7b4bb9
harrybark <harrypmw@gmail.com>
2022-06-07 23:53:26 +0900

    feat. Entity Class 회원, 주문, 아이템, 배달

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // Order class의 Member 클래스의 멤버 변수를 연관관계 설정
+    private List<Order> orders = new ArrayList<>();
+}

c64e4d561a214530a5de81e649af161741a37403
harrybark <harrypmw@gmail.com>
2022-06-09 23:11:53 +0900

    feat Entity추가 Address - 주소관련 Category, Item

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,13 @@ public class Address {
     private String city;
     private String streed;
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String streed, String zipcode) {
+        this.city = city;
+        this.streed = streed;
+        this.zipcode = zipcode;
+    }
 }

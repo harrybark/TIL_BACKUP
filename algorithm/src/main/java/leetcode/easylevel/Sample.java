5ba8652dcc46b3407587e684afb3fab89c303d16
harrybark <harrypmw@gmail.com>
2022-06-21 23:41:40 +0900

    feat.view 반영 1.home 2.static resource

--- algorithm/src/main/java/leetcode/easylevel/Sample.java
+++ algorithm/src/main/java/leetcode/easylevel/Sample.java
@@ -1,2 +1,14 @@
-package leetcode.easylevel;public class Sample {
+package leetcode.easylevel;
+
+public class Sample {
+
+    public static void main(String[] args) {
+
+        String temp = "() ([)] [] {}" ;
+        temp = temp.replaceAll("\\(\\)", "");
+        temp = temp.replaceAll("\\{}", "");
+        temp = temp.replaceAll("\\[]", "");
+        System.out.println("temp : " + temp);
+    }
+    
 }

895335925b02fbef1ea841f8b4ff455f9171e730
harrybark <harrypmw@gmail.com>
2022-07-20 23:29:41 +0900

    feat.자바 ORM.표준 JPA 프로그래밍:다대다

---
+++ ex1-hello-jpa/src/main/java/hellojpa/다대다/Member.java
@@ -0,0 +1,46 @@
+package hellojpa.다대다;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "MEMBER")
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1, allocationSize = 50
+)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    private String username;
+
+    @ManyToMany
+    @JoinTable(name = "MEMBER_PRODUCT" )
+    private List<Product> products = new ArrayList<>();
+
+    public Member() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}

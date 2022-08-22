079fe4dd2d05367bd60098174373ba6b46543537
harrybark <harrypmw@gmail.com>
2022-08-22 22:58:28 +0900

    feat.김영한, 실전데이터 JPA_BULK QUERY, EntityGraph, JPA Hint,Lock

--- data_jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data_jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -13,6 +13,8 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -29,6 +31,9 @@ class MemberRepositoryTest {
 
     @Autowired
     TeamRepository teamRepository;
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     public void testMember() throws Exception {
@@ -197,5 +202,91 @@ class MemberRepositoryTest {
         assertThat(memberPage.getTotalPages()).isEqualTo(2);
         assertThat(memberPage.isFirst()).isTrue();
         assertThat(memberPage.hasNext()).isTrue();
+    }
+    
+    @Test
+    public void bulk_실행한다() throws Exception {
+        // given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 21));
+        memberRepository.save(new Member("member5", 40));
+        // when
+        int resultCount = memberRepository.bulkAgePlus(20);
+        /*
+        영속성 컨텍스트에는 반영이 안되기 때문에 flush, clear를 해준다.
+        em.flush();
+        em.clear();
+        */
+
+        // then
+        assertThat(resultCount).isEqualTo(3);
+        
+    }
+
+    @Test
+    public void findMemberLazy() throws Exception {
+        // given
+        //member1 -> teamA
+        //member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> members = memberRepository.findAll();
+
+        for (Member member : members) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.teamClass = " + member.getTeam().getClass());
+            System.out.println("team = " + member.getTeam().getName());
+        }
+
+        System.out.println("----------------------------");
+        List<Member> memberFetchJoin = memberRepository.findMemberFetchJoin();
+        for (Member member : memberFetchJoin) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.teamClass = " + member.getTeam().getClass());
+            System.out.println("team = " + member.getTeam().getName());
+        }
+
+        List<Member> memberGraphList
+                = memberRepository.findEntityGraphByUsername("member1");
+        for (Member member : memberGraphList) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.teamClass = " + member.getTeam().getClass());
+            System.out.println("team = " + member.getTeam().getName());
+        }
+        // then
+    }
+
+    @Test
+    public void queryHint() throws Exception {
+        // given
+        Member member1 = new Member("member1", 10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        // when
+        Member findMember = memberRepository.findReadOnlyByUsername("member1");
+        findMember.setUsername("member2");
+
+        em.flush();
+
+        // then
+
     }
 }

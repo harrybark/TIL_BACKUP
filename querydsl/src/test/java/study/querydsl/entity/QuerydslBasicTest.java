25a302da8493513ffc24906f5681469158f66557
harrybark <harrypmw@gmail.com>
2022-09-20 23:30:49 +0900

    김영한_Querydsl Join, Join_On

--- querydsl/src/test/java/study/querydsl/entity/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -239,4 +239,95 @@ class QuerydslBasicTest {
         assertThat(fetch.get(0).get(member.age.avg())).isEqualTo(15);
         assertThat(fetch.get(1).get(member.age.avg())).isEqualTo(35);
     }
+
+    @Test
+    public void 조인() throws Exception {
+        // given
+
+        // when
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                //.leftJoin(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        // then
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    @Test
+    public void 세타조인() throws Exception {
+        // given
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> memberList = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+        // then
+        assertThat(memberList)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
+
+
+    /***
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * @throws Exception
+     */
+    @Test
+    public void join_on_filtering() throws Exception {
+        // given
+        
+        // when
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("teamA"))
+                .fetch();
+
+        // then
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+        
+    }
+
+    /***
+     * 연관관계 없음
+     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+     * @throws Exception
+     */
+    @Test
+    public void 조인_연관관계_없는경우() throws Exception {
+        // given
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .fetch();
+
+        // then
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 }
